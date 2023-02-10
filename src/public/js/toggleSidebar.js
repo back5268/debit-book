b0ec7c712e2842b7baa932b8c4bc4223f0d32dad
bachtv1509 <bachtv150902@gmail.com>
2023-02-10 21:35:26 +0700

    add scss

--- src/public/js/toggleSidebar.js
+++
@@ -1,8 +0,0 @@
-const sidebar = document.querySelector(".sidebar");
-const header = document.querySelector(".header");
-const toggle = document.querySelector(".toggle");
-
-toggle.addEventListener("click", () => {
-    sidebar.classList.toggle("close");
-    header.classList.toggle("active");
-  })
